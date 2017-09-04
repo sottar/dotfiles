0d23876ef7653e4a1854a09a326e76c6c763740e
sottar <ohrst.18@gmail.com>
2017-09-05 00:40:07 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,9 @@ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 mkdir ~/.vim & cd ~/.vim
 mkdir colors
 git clone git@github.com:jpo/vim-railscasts-theme.git
-ln -sf vim-railscasts-theme/colors/railscasts.vim ~/.vim/colors/railscasts.vim
+mkdir  ~/.vim/
+mkdir ~/.vim/colors/
+cp vim-railscasts-theme/colors/railscasts.vim ~/.vim/colors/railscasts.vim
 
 # symlinks
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
@@ -28,6 +30,8 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 # ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 # ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 # ln -sf ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+mkdir ~/oh-my-zsh
+mkdir ~/oh-my-zsh/themes
 ln -sf ~/dotfiles/oh-my-zsh/original.zsh-theme ~/oh-my-zsh/themes/original.zsh-theme
 
 source ~/.zshrc & source ~/.zprofile
