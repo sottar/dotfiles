e84f7a3e8aaa1c8cf850ede028028d03bc7e2af0
sottar <ohrst.18@gmail.com>
2018-02-06 17:33:47 +0900

    add eslintignore file

--- setup.sh
+++ setup.sh
@@ -31,6 +31,7 @@ ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 # ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 # ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+# ln -sf ~/dotfiles/vscode/.eslintignore ~/Library/Application\ Support/Code/User/.eslintignore
 # ln -sf ~/dotfiles/vscode/snippets/* ~/Library/Application\ Support/Code/User/snippets/
 # ln -sf ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 mkdir ~/oh-my-zsh
