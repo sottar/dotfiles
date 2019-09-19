8f0ff51912ab48b19bcdf87b4014a9d0d585b34d
sottar <ohrst.18@gmail.com>
2019-09-19 12:30:08 +0900

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -21,12 +21,15 @@ brew tap caskroom/versions
 brew cask install alfred
 brew cask install bettertouchtool
 brew cask install clipy
+brew cask install docker
+brew cask install figma
 brew cask install firefox
 brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install iterm2
+brew cask install karabiner-elements
 brew cask install sequel-pro
 brew cask install sketch
 brew cask install slack
-brew cask install sublime-text
 brew cask install visual-studio-code
+brew cask install zeplin
